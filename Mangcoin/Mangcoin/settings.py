12ee206a4dc25539831c468ae44d4e5ed3576a0a
PinillosDev <pinillosdev@gmail.com>
2022-03-29 18:14:27 -0500

    Apps created added to setting.py

--- Mangcoin/Mangcoin/settings.py
+++ Mangcoin/Mangcoin/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps created
+    'apps.tokens',
+    'apps.transactions',
 ]
 
 MIDDLEWARE = [
