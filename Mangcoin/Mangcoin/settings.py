6cbcba8354b992b875d06ebdf2c4d57ea91e8d33
PinillosDev <pinillosdev@gmail.com>
2022-04-01 15:31:59 -0500

    Models created

--- Mangcoin/Mangcoin/settings.py
+++ Mangcoin/Mangcoin/settings.py
@@ -143,3 +143,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATE_INPUT_FORMATS = ['%m-%d-%Y'] # Is the best way to change the data format of an input
+# DATE_INPUT_FORMATS will be used on the apps' models.
--- Mangcoin/apps/transactions/models.py
+++ Mangcoin/apps/transactions/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from ...Mangcoin import settings
+from ..tokens.models import tokens
 
-# Create your models here.
+PLATFORMS = (
+    ("Wallet"),
+    ("Exchange point"),
+    ("DeFi"),
+    ("somwhere")
+)
+
+class transactions(models.Model):
+    transaction_id = models.BigAutoField(primary_key=True)
+    transaction_date = models.DateField(input_formats=settings.DATE_INPUT_FORMATS)
+    origin = models.CharField(choices=PLATFORMS, default="somwhere")
+    destiny = models.CharField(choices=PLATFORMS, default="somwhere")
+    money_used = models.PositiveBigIntegerField(blank=True)
+    token = models.ForeignKey(tokens, on_delete=models.CASCADE)
+    token_aumont = models.PositiveIntegerField()
+    currently_token_price = models.PositiveSmallIntegerField()
+    description = models.TextField()
